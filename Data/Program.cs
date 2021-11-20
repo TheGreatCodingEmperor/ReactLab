ca9b95178df86947fd463f7b4ab8d06f92e18c6f
TheGreatCodingEmperor <thehandsomeboyno.1@gmail.com>
2021-11-21 00:28:11 +0800

    Data appsettings json with bug

--- Data/Program.cs
+++ Data/Program.cs
@@ -1,15 +1,22 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Pomelo.EntityFrameworkCore.MySql;
-using System.Linq;
 using Newtonsoft.Json;
+using Pomelo.EntityFrameworkCore.MySql;
 
 namespace Data {
     class Program {
         static void Main (string[] args) {
-            var connectionString = "server=127.0.0.1;port=3307;user id=root;password=70400845;database=ReactLab;charset=utf8;";
+            IConfiguration config = new ConfigurationBuilder()
+           .AddJsonFile("appsettings.json", optional:true,reloadOnChange: true)
+           .Build();
+
+
+            string connectionString = config.GetValue<string>("DBConnection:ReactLab");
+            Console.WriteLine(connectionString);
             // var serverVersion = new MySqlServerVersion (new Version (8, 0, 27));
 
             var servicesProvider =
@@ -19,11 +26,11 @@ namespace Data {
                 .AddDbContext<ReactLabContext> (
                     dbContextOptions => dbContextOptions
                     .UseMySql (connectionString))
-                    //建立 service provider
-                    .BuildServiceProvider ();
-                    //從 service provider 中取得上面設定建立的 Runner(YowkoRunner) 服務
-                    var runner = servicesProvider.GetRequiredService<ReactLabContext> ();
-                Console.WriteLine(JsonConvert.SerializeObject(runner.Store.ToList(),Formatting.Indented));
+                //建立 service provider
+                .BuildServiceProvider ();
+            //從 service provider 中取得上面設定建立的 Runner(YowkoRunner) 服務
+            var runner = servicesProvider.GetRequiredService<ReactLabContext> ();
+            Console.WriteLine (JsonConvert.SerializeObject (runner.Store.ToList (), Formatting.Indented));
         }
     }
 }
